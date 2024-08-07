da6685de1adf6e7acb5fc3d45978ac60ae31b713
muzammil868 <hafizmuzammil681@gmail.com>
2024-08-07 08:41:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// alert('Hello, World');
+
+
+// a = 10;
+// console.log(a)
+
+// var a;
+
+// function m(){
+//     console.log("hello world")
+// }
+// m();
+
+// var a = 10;
+
+// function m(){
+// var b = 20;
+//     console.log(a)
+// }
+
+// console.log(b)
+// m();
+// m(2,3)
+
+// function m(a,b){
+// console.log(a * b)
+// }
+
+// function m(a,b){
+// console.log(a+b)
+// }
+
+// // arrow function 
+// const ab = ()=>{
+//     console.log ("hello arrow function")
+// }
+// ab();
+// !function(){
+//     console.log("hello world")
+// }();
+
+// (function(){
+//     console.log("hello world")
+// }())
+
+var arr = [1,2,6,4,5]
+// console.log (arr)
+// arr.push(8,9)
+// console.log (arr)
+// arr.pop()
+// console.log (arr)
+
+// arr[3] = 100
+// console.log(arr)
+
+// arr.unshift("ali", "raza")
+
+// arr.splice(0,2,"ali","raza")
+// console.log(arr)
+
+
+
+// var srt = arr.sort();
+// console.log(srt)
+
+
+// var rev = arr.reverse();
+// console.log(rev)
+
